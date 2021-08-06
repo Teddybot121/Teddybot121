c32c2e142519942a05b512307d59e028a7d8fe9e
Teddybot121 <88265036+Teddybot121@users.noreply.github.com>
2021-08-06 23:47:41 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -814,7 +814,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                                     '\n*Teddy an error has occurred!*'+
                                     '\n*This error log may include your number or the number of an opponent. Please be careful with it!*' +
                                     '\n*You can write to our Telegram group for help.*' +
-                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/LIT28iiU5vw3OHPC4Vn6q5'
+                                    '\n*Aslo you can join our support group:* https://chat.whatsapp.com/LIT28iiU5vw3OHPC4Vn6q5' +
                                     '\n*This message should have gone to your number (saved messages).*\n\n' +
                                     '*Error:* ```' + error + '```\n\n'
                                     , MessageType.text, {detectLinks: false}
